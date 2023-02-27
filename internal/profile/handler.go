c8cb7923722aeb67126fc656a7252ff9966565ac
fabijan <fabijan@thefootballcompany.com>
2023-02-27 20:24:31 +0100

    Profile endpoint for current user

--- internal/profile/handler.go
+++ internal/profile/handler.go
@@ -1,12 +1,14 @@
 package profile
 
 import (
+	"net/http"
+	"strconv"
+
 	"github.com/gin-gonic/gin"
 	"github.com/kollektive-hackathon/battleblocks-backend/internal/pkg/middleware"
 	"github.com/kollektive-hackathon/battleblocks-backend/internal/pkg/reject"
+	"github.com/kollektive-hackathon/battleblocks-backend/internal/pkg/utils"
 	"gorm.io/gorm"
-	"net/http"
-	"strconv"
 )
 
 type profileHandler struct {
@@ -19,18 +21,14 @@ func RegisterRoutes(rg *gin.RouterGroup, db *gorm.DB) {
 	}
 
 	routes := rg.Group("/profile")
-	routes.GET("/:id", middleware.VerifyAuthToken, handler.getProfileById)
+	routes.GET("/", middleware.VerifyAuthToken, handler.getProfile)
 	routes.PUT("/:id/blocks", middleware.VerifyAuthToken, handler.activateBlock)
 }
 
-func (h profileHandler) getProfileById(c *gin.Context) {
-	id, parseErr := strconv.ParseUint(c.Param("id"), 0, 64)
-	if parseErr != nil {
-		c.JSON(http.StatusBadRequest, reject.RequestParamsProblem())
-		return
-	}
+func (h profileHandler) getProfile(c *gin.Context) {
+	email := utils.GetUserEmail(c)
 
-	profile, err := h.profile.FindById(id)
+	profile, err := h.profile.FindByEmail(email)
 	if err != nil {
 		c.JSON(err.Problem.Status, err.Problem)
 		return
