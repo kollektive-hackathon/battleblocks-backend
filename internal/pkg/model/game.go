79f3854a058e2a73a98ffab3cc2ec2e429bf2f05
fabijan <fabijan@thefootballcompany.com>
2023-02-28 13:43:16 +0100

    Game struct json tags

--- internal/pkg/model/game.go
+++ internal/pkg/model/game.go
@@ -1,15 +1,15 @@
 package model
 
 type Game struct {
-	Id           uint64
-	FlowId       *uint64
-	OwnerId      uint64
-	ChallengerId *uint64
-	GameStatus   GameStatus
-	Stake        uint64
-	TimeStarted  int64
-	TimeCreated  int64
-	WinnerId     *uint64
+	Id           uint64     `json:"id"`
+	FlowId       *uint64    `json:"flowId"`
+	OwnerId      uint64     `json:"ownerId"`
+	ChallengerId *uint64    `json:"challengerId"`
+	GameStatus   GameStatus `json:"gameStatus"`
+	Stake        uint64     `json:"stake"`
+	TimeStarted  int64      `json:"timeStarted"`
+	TimeCreated  int64      `json:"timeCreated"`
+	WinnerId     *uint64    `json:"winnerId"`
 }
 
 func (Game) TableName() string {
